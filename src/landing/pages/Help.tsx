963f18d93b6a8496711812a857418ddc8a2bba10
SEBASTIAN PALACIO HERNANDEZ <spalacioh@ucundinamarca.edu.co>
2024-04-12 19:35:39 -0500

    style(Create ChatBot): :lipstick:

--- src/landing/pages/Help.tsx
+++ src/landing/pages/Help.tsx
@@ -1,7 +1,9 @@
+import ChatBotComponent from "../components/ChatBotComponent/ChatBotComponent";
+
 const Help = () => {
   return (
-    <div className="flex justify-center items-center min-h-screen text-7xl animate-bounce text-cyan-950 dark:text-indigo-100">
-      ¡No hacerlo!
+    <div>
+      <ChatBotComponent/>
     </div>
   );
 };
