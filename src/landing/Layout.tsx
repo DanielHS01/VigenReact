528565db3f9b4f41f3e5299d41edc209bd5d73bd
SEBASTIAN PALACIO HERNANDEZ <spalacioh@ucundinamarca.edu.co>
2024-11-30 12:19:10 -0500

    style: :lipstick: Realización de diseño ChatBot
    Se hizo cambio del chatbot, se cambió el punto de visualización, se agregó animación y se corrigieron errores de logica en la programación anteriormente hecha

--- src/landing/Layout.tsx
+++ src/landing/Layout.tsx
@@ -1,12 +1,14 @@
 import { Outlet, ScrollRestoration } from "react-router-dom";
 import Header from "@/landing/components/Header/Header";
 import Footer from "@/landing/components/Footer/Footer";
+import ChatBotComponent from "./components/ChatBotComponent/ChatBotComponent";
 
 const Layout = () => {
   return (
     <div className="bg-cyan-50 dark:bg-cyan-950">
       <Header />
       <Outlet />
+      <ChatBotComponent/>
       <Footer />
       <ScrollRestoration />
     </div>
