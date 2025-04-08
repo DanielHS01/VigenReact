b1b5a17978b177ae1f36011a8e54e2ddeb3d7981
DanielHS <danielhs2908@gmail.com>
2025-04-08 18:17:48 -0500

    fix:

--- src/organization/components/Edit/Password.tsx
+++ src/organization/components/Edit/Password.tsx
@@ -1,10 +1,7 @@
 import FormContainer from "@/shared/ui/FormContainer";
 import Input from "@/shared/ui/Input";
-import { useTranslation } from "react-i18next";
 
 const Password = () => {
-  const { t } = useTranslation();
-
   return (
     <FormContainer className="space-y-5 px-10">
       <p className="text-center">Cambiar Contraseña</p>
