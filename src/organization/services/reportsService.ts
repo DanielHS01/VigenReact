b4dca954b2f3cfc7e3aacf01621f215c58be9238
DanielHS <danielhs2908@gmail.com>
2025-04-26 20:55:59 -0500

    fix: :bug: Reports Request

--- src/organization/services/reportsService.ts
+++ src/organization/services/reportsService.ts
@@ -1,12 +1,8 @@
 // src/services/userServices.ts
 
 export const downloadUserReport = async () => {
-    const response = await fetch(`https://vigenbackend.up.railway.app/api/User/reporte/pdf`, {
+    const response = await fetch(`https://vigenbackend.up.railway.app/api/user/reporte/pdf`, {
       method: "GET",
-      headers: {
-        "Content-Type": "application/pdf",
-        Authorization: `Bearer ${localStorage.getItem("token")}`, // Si usas token, si no, puedes quitar esto
-      },
     });
   
     if (!response.ok) {
@@ -24,12 +20,8 @@ export const downloadUserReport = async () => {
   };
   
 export const downloadNotifyReport = async () => {
-  const response = await fetch(`https://vigenbackend.up.railway.app/api/Notify/reporte/pdf`, {
+  const response = await fetch(`https://vigenbackend.up.railway.app/api/notify/reporte/pdf`, {
     method: "GET",
-    headers: {
-      "Content-Type": "application/pdf",
-      Authorization: `Bearer ${localStorage.getItem("token")}`, // Si usas token, si no, puedes quitar esto
-    },
   });
 
   if (!response.ok) {
